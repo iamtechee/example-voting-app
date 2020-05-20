14571ed16f019941b2a3206b4ce29631f1397a93
iamtechee <65681115+iamtechee@users.noreply.github.com>
2020-05-20 18:08:07 -0500

    Update server.js

--- result/server.js
+++ result/server.js
@@ -70,7 +70,12 @@ function collectVotesFromResult(result) {
 }
 
 app.use(cookieParser());
-app.use(bodyParser());
+//app.use(bodyParser());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
